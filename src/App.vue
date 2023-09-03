2caea9b5c30f6575e28049f7f5c6eb6c591db861
BITNA GU <126743003+starirene9@users.noreply.github.com>
2023-09-03 13:40:36 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- template 안에 HTML -->
+  <img alt="Vue logo" src="./assets/logo.png">
+  Hello World!
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+//JS 작성
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
