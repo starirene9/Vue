6a28ac9793410a1f0ee5a3377881d77dfac67a72
BITNA GU <126743003+starirene9@users.noreply.github.com>
2023-09-06 01:33:14 +0900

    v-if & Modal true & false practice

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <!-- template 안에 HTML -->
 
   <div class="nav">
     <a href="" v-for="(data, i) in nav" :key="i">{{data}}</a>
@@ -7,25 +6,27 @@
     <!-- <태그 v-for="작명 in 몇회" :key="작명" > key 꼭 써줘야 에러 안남-->
     <!-- key의 용도는? 반복문 돌린 요소를 컴퓨터가 구분하기 위해 씀 i 는 index 비슷하게 사용 됨: 0 1 2 -->
   </div>
+  
+  <div class="black-bg" v-if="(modal == true)">
+  <div class="white-bg">
+    <h4>상세페이지임</h4>
+    <p>상세페이지 내용임</p>
+    <button @click="modal = false">창 닫기</button>
+  </div>
+</div>
+
   <!-- 반복문 돌리기 -->
-  <h1>원룸샵</h1>
+  <h1>팅커벨의 방 구하기</h1>
   <div v-for="(area, i) in areas" :key="i">
+    <img @click="modal = true" src= "./assets/room0.jpg" alt="원룸방" class="photo">
     <h4>{{area}}</h4>
-    <p>전세 {{prices[i]}}만원</p>
+    <p>전세 {{prices[i]}}만원</p> 
+    <button @click="신고수[i]++">허위매물신고</button> <span>신고수 : <em>{{신고수[i]}}</em></span>
   </div>
-  <!-- <div>
-    <h1>원룸샵</h1>
-    <h4 :style="스타일">{{areas[0]}}</h4>
-    <p>전세 {{price1}}만원</p>
-  </div>
-  <div>
-    <h4>{{areas[1]}}</h4>
-    <p>전세 {{price2}}만원</p>
-  </div>
-  <div>
-    <h4>{{areas[2]}}</h4>
-    <p>전세 {{price3}}만원</p>
-  </div> -->
+
+<!-- v-on:click="자바스크립트(코드가 길어지면 아래 함수로 만들어서 사용하기)" -->
+<!-- @click @mouseover @input 기타 등등 이벤트 명이 있음 -->
+
 </template>
 
 <script>
@@ -43,18 +44,28 @@ export default {
 
   data(){
     return{
+      modal : false,
       prices : [4500, 7000, 10000],
       스타일 : 'color : orange',
       nav : ['Home', 'Products', 'About'],
-      areas : ['구갈동 원룸', '신갈동 원룸', '동천동 오피스텔'],
+      areas : ['신갈동 원룸', '수지구청 원룸', '미금역 오피스텔'],
+      신고수 : [0,0,0],
     }
   },
+  methods : {
+    // increase() {
+    // 사용시에는 increase 만 쓰면 됨 
+    // this.신고수 += 1;
+    // }
+  },
+
   components: {
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+// @import "@/asstes/scss/파일명";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -63,8 +74,15 @@ export default {
   color: #2c3e50;
 }
 
+body {
+  margin : 0;
+}
+div {
+  box-sizing: border-box;
+}
+
 .nav {
-  background: darkslateblue;
+  background: rgb(139, 61, 108);
   padding: 15px;
   border-radius: 5px;
 }
@@ -77,4 +95,33 @@ export default {
   font-size: 1.2rem;
   letter-spacing: 1px;
 }
+
+.photo {
+  width: 60%;
+  margin-top: 40px;
+  cursor: pointer;
+}
+
+button {
+  cursor: pointer;
+  border-radius: 5px;
+}
+
+// 모달창 : html, css 먼저 만들기 
+// 동적이 UI 만들기 
+// 1. UI 현재 상태를 데이터로 저장하기 
+// 2. 데이터에 따라 UI가 어떻게 보일지 작성 
+
+.black-bg {
+  width: 100%; 
+  height:100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+} 
+
 </style>
